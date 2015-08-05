9eac2f9d8dbefa5d3fa1081144369455f301fc9a
Mario Inga Cahuana <mario.inga@clicksandbricks.pe>
2015-08-05 17:39:11 -0500

    Ready

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+myimage="busybox"
+
+docker pull $myimage:latest
+docker export $(docker create $myimage) > busybox.tar
+mkdir rootfs
+tar -C rootfs -xf $myimage.tar
+rm -f $myimage.tar
+
+sudo runc
